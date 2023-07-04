f23dc8b052f80bc05a62d8ccecaf8cd0d8c15d81
Abraham J. Ruiz R <major.tom.spaceworm@gmail.com>
2023-07-04 15:13:31 +0200

    :hammer: verbose ft wip

--- .duckwizard/scripts/config.mk
+++ .duckwizard/scripts/config.mk
@@ -5,6 +5,8 @@ PARSE_CONFIG               = $(SCRIPTS_DIR)/parse_config $(CONFIG_FILE)
 ### project configuration ###
 PROJECT                           ?= $(shell $(PARSE_CONFIG) project)
 TOP_MODULE                        ?= $(shell $(PARSE_CONFIG) rtl_top)
+# - duckwizard verbose
+DW_VERBOSE                        ?= $(shell $(PARSE_CONFIG) dw_verbose)
 # - project-features
 RTL_LINTER                        ?= $(shell $(PARSE_CONFIG) rtl_linter)
 RTL_SYN_TOOLS                     ?= $(shell $(PARSE_CONFIG) rtl_synth_tools)
